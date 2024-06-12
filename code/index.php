7f19b5a00fa780bce73e0e91f3ced45bc87df89e
bitmunu <120228179+bitmunu@users.noreply.github.com>
2024-06-12 16:13:00 +0300

    Update index.php
    2.2

--- code/index.php
+++ code/index.php
@@ -1,14 +1,41 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $surname = $_POST["lastname"];
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+
+    $_SESSION["lastname"] = $surname;
+    $_SESSION["name"] = $name;
+    $_SESSION["age"] = $age;
+
+    header("Location: help.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>text</title>
+    <title>form</title>
 </head>
+
 <body>
-<form method="post" action="help.php">
-    <textarea name="text" rows="4"></textarea>
-    <input type="submit" value="result">
+
+<form method="post">
+    <label for="surname">фамилия:</label>
+    <input type="text" id="lastname" name="lastname" required><br><br>
+    <label for="name">имя:</label>
+    <input type="text" id="name" name="name" required><br><br>
+    <label for="age">годиков:</label>
+    <input type="number" id="age" name="age" required><br><br>
+
+    <input type="submit" value="Отправить">
 </form>
+
 </body>
 </html>
